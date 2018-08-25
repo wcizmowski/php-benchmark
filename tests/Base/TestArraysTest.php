b6565350dae0c6d1e27f5e31c693bc9fb5e74eeb
Witold Ciżmowski <witek@iwebi.pl>
2018-08-26 01:14:21 +0200

    Test Arrays improvements

--- tests/Base/TestArraysTest.php
+++ tests/Base/TestArraysTest.php
@@ -11,11 +11,20 @@ require_once __DIR__ . '/../../Benchmark/TestArrays.php';
 class TestArraysTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     *  TestArrays object creating
      */
     public function testReturnObject()
     {
         $testMath = new \TestsArrays\TestArrays();
 
         $this->assertInternalType('object', $testMath);
+    }
+
+    public function testNotEmptyResult()
+    {
+        $testMath = new \TestsArrays\TestArrays();
+        $testMath->Test($result);
+
+        $this->assertNotEquals($result, '');
     }
 }
