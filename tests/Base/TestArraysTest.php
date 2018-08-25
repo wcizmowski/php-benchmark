9d9507035b79e8e8a903cc56f77b155b6defb993
Witold Ciżmowski <witek@iwebi.pl>
2018-08-26 00:42:08 +0200

    Test Arrays Added

---
+++ tests/Base/TestArraysTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Base;
+
+require_once __DIR__ . '/../../Benchmark/TestArrays.php';
+
+/**
+ * Class OffsetEncodingAlgorithmTest
+ * @package Base
+ */
+class TestArraysTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param $result
+     * @param int $count
+     */
+    public function testTest()
+    {
+        $testMath = new \TestsArrays\TestArrays();
+
+        $this->assertInternalType('object', $testMath);
+    }
+}
